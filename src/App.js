1941fe4866c14f013a95d2e6c88a009381384469
88JumpingJacks <jackL8@outlook.com>
2018-02-18 17:13:04 -0500

    Week 1 - Routing, input field

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Title = (props) => {
+  return (
+    <div>
+      <h1>Router Demo</h1>
+      <Link to='/list'><button>List</button></Link>
+      <form>
+        Input yo: <input type="text" onChange={props.handleChange}/>
+      </form>
+    </div>
+  );
+}
+
+const List = () => {
+  return (
+    <div>
+      <h1>List</h1>
+      <Link to='/'><button>Title</button></Link>
+    </div>
+  );
+}
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(e) {
+    console.log(e.target.value);
+    
+    if (e.target.value.charAt(0).toLowerCase() == 'b') {
+      alert("Starts with a b!");
+
+      // Week 2: pass state to component that renders message
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <h1>h8 bind()</h1>
+          <input type="text" onChange = {this.handleChange} placeholder = "text in bind"/>
+          <ul>
+            <Link to='/'>Title </Link>
+            <Link to='/list'>List</Link>
+          </ul>
+          <Route exact path='/' render={(props) => (<Title handleChange={this.handleChange}/>)} />
+          <Route path='/list' component={List}/>
+        </div>
+      </Router>
     );
   }
 }
